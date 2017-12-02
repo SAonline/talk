8a51301e5e5bd725456942828e2804f3b0daf32e
Don Alcuizar <dalcuizar@staradvertiser.com>
2017-12-01 14:58:44 -1000

    Fix issue with email change and id change

--- plugins/talk-plugin-auth-sa/server/notfound.js
+++ plugins/talk-plugin-auth-sa/server/notfound.js
@@ -1,8 +1,6 @@
 const UserModel = require('../../../models/user');
 
 module.exports = { tokenUserNotFound: async ({jwt, token}) => {
-    console.log("NOT FOUND2");
-    console.log("JWT:" + JSON.stringify(jwt));
 
     let profile = jwt;
 	
@@ -10,37 +8,99 @@ module.exports = { tokenUserNotFound: async ({jwt, token}) => {
 		return null;
 	}
 
+	if( !profile.username)
+	{
+		return null;
+	}
 	profile.username = profile.username.replace(' ', '_');
 	profile.username = profile.username.replace(/\W/g, '');
 
-	let user = await UserModel.findOneAndUpdate({
-		id: profile.sub
-	}, {
-		id: profile.sub,
-		username: profile.username,
-		lowercaseUsername: profile.username.toLowerCase(),
-		roles: [],
-		profiles: [
-			{
-				id: profile.email,
-				provider: 'local',
-				metadata : {
-					confirmed_at : new Date()
-				}
-			}
+	let dbUser = await UserModel.findOne({
+		$or: [
+			{ 'profiles': { $elemMatch: { 'id': profile.email } } },
+			{ 'id': profile.subid }
 		]
-	}, {
-		setDefaultsOnInsert: true,
-		new: true,
-		upsert: true
 	});
 
-	if( profile.username.toLowerCase()  === user.username.toLowerCase() )
+	if( !dbUser ) 
 	{
-		console.log("Display Name is different");
+		let user = await UserModel.findOneAndUpdate({
+			$or: [
+				{ 'profiles': { $elemMatch: { 'id': profile.email } } },
+				{ 'id': profile.subid }
+			]
+		}, {
+			id: profile.subid,
+			username: profile.username,
+			lowercaseUsername: profile.username.toLowerCase(),
+			roles: [],
+			profiles: [
+				{
+					id: profile.email,
+					provider: 'local',
+					metadata : {
+						confirmed_at : new Date()
+					}
+				}
+			]
+		}, {
+			setDefaultsOnInsert: true,
+			new: true,
+			upsert: true
+		});
+
+		return user;
+
+	} else {
+
+		if( ( profile.subid && dbUser.id != profile.subid ) )
+		{
+			let dbUser = await UserModel.findOneAndUpdate({
+				'profiles': { $elemMatch: { 'id': profile.email } }
+			}, {
+				id: profile.subid
+			}, {
+				upsert: false,
+				returnNewDocument: true
+			});
+		} 
+
+		if ( profile.email && dbUser.profiles[0].id != profile.email ) 
+		{
+			let dbUser = await UserModel.findOneAndUpdate({
+				'id': profile.subid 
+			}, {
+				profiles: [
+					{
+						id: profile.email,
+						provider: 'local',
+						metadata : {
+							confirmed_at : new Date()
+						}
+					}
+				]
+			}, {
+				upsert: false,
+				returnNewDocument: true
+			});
+		}
+
+		if( profile.username && dbUser.username != profile.username )
+		{
+			let dbUser = await UserModel.findOneAndUpdate({
+				'profiles': { $elemMatch: { 'id': profile.email } }
+			}, {
+				username: profile.username,
+				lowercaseUsername: profile.username.toLowerCase()
+			}, {
+				upsert: false,
+				returnNewDocument: true
+			});
+		} 
+			
+		return dbUser;
 	}
 
-	return user;
 
   }
 };
