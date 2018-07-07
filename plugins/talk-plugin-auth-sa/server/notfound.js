0cf5e4bc3ece2e1a644bc2ac126f8601d39c7f8d
Don Alcuizar <dalcuizar@staradvertiser.com>
2018-07-06 15:41:44 -1000

    Temp Fix #1703

--- plugins/talk-plugin-auth-sa/server/notfound.js
+++ plugins/talk-plugin-auth-sa/server/notfound.js
@@ -1,112 +1,145 @@
 const UserModel = require('../../../models/user');
 
-module.exports = { tokenUserNotFound: async ({jwt, token}) => {
-
+module.exports = {
+  tokenUserNotFound: async ({ jwt, token }) => {
     let profile = jwt;
-	
-	if( !profile || !profile.username || !profile.email || !profile.subid )
-	{
-		return null;
-	}
-	
-	profile.username = profile.username.replace(' ', '_');
-	profile.username = profile.username.replace(/\W/g, '');
 
-	let dbUser = await UserModel.findOne({
-		$or: [
-			{ 'id': profile.subid },
-			{ 'profiles': { $elemMatch: { 'id': profile.email } } }
-		]
-	});
+    if (
+      !profile ||
+      !profile.username ||
+      !profile.email ||
+      !profile.subid ||
+      !token
+    ) {
+      return null;
+    }
 
-	if( !dbUser ) 
-	{
-		let user = await UserModel.findOneAndUpdate({
-			$or: [
-				{ 'profiles': { $elemMatch: { 'id': profile.email } } },
-				{ 'id': profile.subid }
-			]
-		}, {
-			id: profile.subid,
-			username: profile.username,
-			lowercaseUsername: profile.username.toLowerCase(),
-			roles: [],
-			profiles: [
-				{
-					id: profile.email,
-					provider: 'local',
-					metadata : {
-						confirmed_at : new Date()
-					}
-				}
-			]
-		}, {
-			setDefaultsOnInsert: true,
-			new: true,
-			upsert: true
-		});
+    profile.username = profile.username.replace(' ', '_');
+    profile.username = profile.username.replace(/\W/g, '');
 
-		return user;
+    let dbUser = await UserModel.findOne({
+      $or: [
+        { id: profile.subid },
+        { profiles: { $elemMatch: { id: profile.email } } },
+      ],
+    });
 
-	} else {
+    if (!dbUser) {
+      let user = await UserModel.findOneAndUpdate(
+        {
+          $or: [
+            { profiles: { $elemMatch: { id: profile.email } } },
+            { id: profile.subid },
+          ],
+        },
+        {
+          id: profile.subid,
+          username: profile.username,
+          lowercaseUsername: profile.username.toLowerCase(),
+          roles: [],
+          profiles: [
+            {
+              id: profile.email,
+              provider: 'local',
+              metadata: {
+                confirmed_at: new Date(),
+              },
+            },
+          ],
+          status: {
+            username: {
+              status: 'SET',
+            },
+          },
+        },
+        {
+          setDefaultsOnInsert: true,
+          new: true,
+          upsert: true,
+        }
+      );
 
-		var dbUserUpdated = null;
+      return user;
+    } else {
+      var dbUserUpdated = null;
 
-		if( ( profile.subid && dbUser.id != profile.subid ) )
-		{
-			dbUserUpdated = await UserModel.findOneAndUpdate({
-				'profiles': { $elemMatch: { 'id': profile.email } }
-			}, {
-				id: profile.subid
-			}, {
-				upsert: false,
-				new: true
-			});
-		} 
+      if (profile.subid && dbUser.id != profile.subid) {
+        dbUserUpdated = await UserModel.findOneAndUpdate(
+          {
+            profiles: { $elemMatch: { id: profile.email } },
+          },
+          {
+            id: profile.subid,
+          },
+          {
+            upsert: false,
+            new: true,
+          }
+        );
+      }
 
-		if ( profile.email && dbUser.profiles[0].id != profile.email ) 
-		{
-			dbUserUpdated = await UserModel.findOneAndUpdate({
-				'id': profile.subid 
-			}, {
-				profiles: [
-					{
-						id: profile.email,
-						provider: 'local',
-						metadata : {
-							confirmed_at : new Date()
-						}
-					}
-				]
-			}, {
-				upsert: false,
-				new: true
-			});
-		}
+      if (profile.email && dbUser.profiles[0].id != profile.email) {
+        dbUserUpdated = await UserModel.findOneAndUpdate(
+          {
+            id: profile.subid,
+          },
+          {
+            profiles: [
+              {
+                id: profile.email,
+                provider: 'local',
+                metadata: {
+                  confirmed_at: new Date(),
+                },
+              },
+            ],
+          },
+          {
+            upsert: false,
+            new: true,
+          }
+        );
+      }
 
-		if( profile.username && dbUser.username != profile.username )
-		{
-			dbUserUpdated = await UserModel.findOneAndUpdate({
-				'profiles': { $elemMatch: { 'id': profile.email } }
-			}, {
-				username: profile.username,
-				lowercaseUsername: profile.username.toLowerCase()
-			}, {
-				upsert: false,
-				new: true
-			});
-		} 
-		
-		if( dbUserUpdated )
-		{
-			return dbUserUpdated;
-		} else {
-			return dbUser;
-		}		
-	}
+      if (profile.username && dbUser.username != profile.username) {
+        dbUserUpdated = await UserModel.findOneAndUpdate(
+          {
+            profiles: { $elemMatch: { id: profile.email } },
+          },
+          {
+            username: profile.username,
+            lowercaseUsername: profile.username.toLowerCase(),
+          },
+          {
+            upsert: false,
+            new: true,
+          }
+        );
+      }
 
-
-  }
+      if (!dbUser.status.username.status) {
+        dbUserUpdated = await UserModel.findOneAndUpdate(
+          {
+            profiles: { $elemMatch: { id: profile.email } },
+          },
+          {
+            status: {
+              username: {
+                status: 'SET',
+              },
+            },
+          },
+          {
+            upsert: false,
+            new: true,
+          }
+        );
+      }
+      if (dbUserUpdated) {
+        return dbUserUpdated;
+      } else {
+        return dbUser;
+      }
+    }
+  },
 };
-
-
