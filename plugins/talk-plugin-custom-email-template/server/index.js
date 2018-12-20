fb60160cd8331819d37d2c039567c052562e56e6
Don Alcuizar <dalcuizar@staradvertiser.com>
2018-12-19 17:06:25 -1000

    Fix email-template issue for talk-plugin-custom-email-template

--- plugins/talk-plugin-custom-email-template/server/index.js
+++ plugins/talk-plugin-custom-email-template/server/index.js
@@ -1,9 +1,5 @@
-const path = require('path');
-const translations = path.join(__dirname, 'translations.yml');
-
 const connect = require('./connect');
 
 module.exports = {
   connect,
-  translations,
 };
